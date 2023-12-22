5e9769e298323d7cefe1cbbb5b6d80a7ae5bde7c
sub1ime8 <sub1ime8@yahoo.com>
2023-12-22 12:25:16 -0500

    Test comment2

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  //test comment
+  //test comment 2
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
